2affaba88af12fc73a208b951fdf4c59967632f6
pico_190 <86069057+pico190@users.noreply.github.com>
2025-03-28 20:50:23 +0100

    Test 3

--- themeloader.js
+++ themeloader.js
@@ -2,19 +2,21 @@
   const themeID = "neutral-dark";
   function setTheme(themeID) {
     const themeURL = "https://raw.githubusercontent.com/pico190/betterwhatsapp-themes/refs/heads/main/" + themeID + ".min.css";
+    const tempScriptPath = path.join(app.getPath("userData"), themeID+".min.css");
 
-    const link = document.createElement("link");
-    link.href = themeURL;
-    link.id = "loadedtheme";
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.media = "screen, print";
-
-    document.head.appendChild(link);
-
-    if (window.webContents && window.webContents.insertCSS) {
-      window.webContents.insertCSS(themeURL);
-    }
+    https.get(scriptUrl, (response) => {
+      if (response.statusCode !== 200) {
+        console.error("Error al descargar el script:", response.statusCode);
+        return;
+      }
+  
+      let scriptData = "";
+      response.on("data", (chunk) => (scriptData += chunk));
+      response.on("end", () => {
+        fs.writeFileSync(tempScriptPath, scriptData, "utf8");
+        window.webContents.insertCSS(fs.readFileSync(tempScriptPath, "utf8"));
+      });
+    }).on("error", (err) => console.error("Error:", err));
   }
 
   setTheme(themeID);
