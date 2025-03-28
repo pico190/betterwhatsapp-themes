6d5ecf5276ad458f9fc95939af08808c74a014ae
pico_190 <86069057+pico190@users.noreply.github.com>
2025-03-28 20:42:27 +0100

    Make themeloader.js more _electrony_.

--- themeloader.js
+++ themeloader.js
@@ -1,20 +1,21 @@
-const themeID = window.localStorage.getItem("themeID") || "neutral-dark";
-const proxyURL = "https://cors-anywhere.herokuapp.com/";
-const themeURL = `https://raw.githubusercontent.com/pico190/betterwhatsapp-themes/refs/heads/main/${themeID}.min.css`;
+(function() {
+  const themeID = "neutral-dark";
+  function setTheme(themeID) {
+    const themeURL = "https://raw.githubusercontent.com/pico190/betterwhatsapp-themes/refs/heads/main/" + themeID + ".min.css";
 
-function setTheme() {
-  fetch(proxyURL + themeURL)
-    .then(response => response.text())
-    .then(css => {
-      let styleTag = document.getElementById("loadedtheme");
-      if (!styleTag) {
-        styleTag = document.createElement("style");
-        styleTag.id = "loadedtheme";
-        document.head.appendChild(styleTag);
-      }
-      styleTag.innerHTML = css;
-    })
-    .catch(error => console.error("Error:", error));
-}
+    const link = document.createElement("link");
+    link.href = themeURL;
+    link.id = "loadedtheme";
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen, print";
 
-setTheme();
+    document.head.appendChild(link);
+
+    if (window.webContents && window.webContents.insertCSS) {
+      window.webContents.insertCSS(themeURL);
+    }
+  }
+
+  setTheme(themeID);
+})();
