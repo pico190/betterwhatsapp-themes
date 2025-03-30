b413082cffa1b201b3224dd459dd22c61de61ddb
pico_190 <86069057+pico190@users.noreply.github.com>
2025-03-30 15:34:05 +0200

    Update themeloader.js

--- themeloader.js
+++ themeloader.js
@@ -6,6 +6,65 @@
       name: "Neutral Dark"
     }
   ]
+
+// Fix fonts bug
+function downloadFont(url, dest) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    https.get(url, (response) => {
+      if (response.statusCode !== 200) {
+        console.error("💀 Error al descargar la fuente:", response.statusCode);
+        return reject(new Error(`Status Code: ${response.statusCode}`));
+      }
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(resolve);
+      });
+    }).on('error', (err) => {
+      fs.unlink(dest, () => reject(err));
+    });
+  });
+}
+
+function fontsLoader(window) {
+  const googleFonts = [
+    {
+      url: "https://fonts.gstatic.com/s/inter/v18/UcCm3FwrK3iLTcvnUwkT9nA2.woff2",
+      css: `@font-face{font-family:'Inter';font-style:italic;font-weight:100 900;font-display:swap;src:url($url) format('woff2');unicode-range:U+0460-052F,U+1C80-1C8A,U+20B4,U+2DE0-2DFF,U+A640-A69F,U+FE2E-FE2F;}`
+    },
+    {
+      url: "https://fonts.gstatic.com/s/inter/v18/UcCm3FwrK3iLTcvnUwAT9nA2.woff2",
+      css: `@font-face{font-family:'Inter';font-style:italic;font-weight:100 900;font-display:swap;src:url($url) format('woff2');unicode-range:U+0301,U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116;}`
+    },
+    {
+      url: "https://fonts.gstatic.com/s/inter/v18/UcCm3FwrK3iLTcvnUwgT9nA2.woff2",
+      css: `@font-face{font-family:'Inter';font-style:italic;font-weight:100 900;font-display:swap;src:url($url) format('woff2');unicode-range:U+1F00-1FFF;}`
+    },
+    {
+      url: "https://fonts.gstatic.com/s/inter/v18/UcCo3FwrK3iLTcviYwY.woff2",
+      css: `@font-face{font-family:'Inter';font-style:normal;font-weight:100 900;font-display:swap;src:url($url) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;}`
+    }
+  ];
+
+  const fontPromises = googleFonts.map(font => {
+    const fileName = path.basename(new URL(font.url).pathname);
+    const localFontPath = path.join(app.getPath("userData"), fileName);
+    return downloadFont(font.url, localFontPath)
+      .then(() => {
+        const fileUrl = `file://${localFontPath}`;
+        return font.css.replaceAll("$url", fileUrl);
+      });
+  });
+
+  Promise.all(fontPromises)
+    .then(cssArray => {
+      const fullCSS = cssArray.join("\n");
+      window.webContents.insertCSS(fullCSS);
+    })
+    .catch(err => {
+    });
+}
+  
   const themeID = "neutral-dark"; // Default theme
   function setTheme(themeID) {
     const themeURL = "https://raw.githubusercontent.com/pico190/betterwhatsapp-themes/refs/heads/main/" + themeID + ".min.css";
@@ -27,6 +86,7 @@
   }
 
   setTheme(themeID);
+  fontsLoader(window);
   window.webContents.executeJavaScript(`setInterval(() => {
 
 document.querySelector("title").textContent = "BetterWhatsapp";
